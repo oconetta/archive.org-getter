73555a1ea56bc223342d934a6fea3dd2e6bb1907
oconetta <olivia.conetta@gmail.com>
2013-07-20 12:13:17 -0400

    Added user input

--- archive.org-getter.rb
+++ archive.org-getter.rb
@@ -1,41 +1,52 @@
 #URL opener
 require 'open-uri'
+require 'fileutils'
 
-# set variables
-start = 0
-ROWS = 200
+def write_to_file
 
-json_data = []
-
-# incrementally download JSON files
-while start < 1000
-# don't forget to turn variable numbers into strings with .to_s
-  url = "http://archive.org/details/tv?q=%22Proposition%2034%22&time=20120917-20121112&start=" + start.to_s + "&rows=" + ROWS.to_s + "&output=json"
-  print "fetching from " + start.to_s + "\n"
-  print "  "+url + "\n"
-
+  puts "What do you want to search?"
+  search_term = gets.chomp.to_s
   
-  file = open(url)
-  results = file.read
-  print "  got "+results.length.to_s+" characters\n"
- 
-  #openurl library and pass url to fetch it
-  #if success
-   # increment start row +100
-    #save to disk with filename results startrow#.json
-  #else 
-   # wait one minute and do it again
+  search_term = search_term.gsub!(" ", "%20")
 
-# if page doesn't load...
-  if results.index("Our Search Engine was not responsive.") == nil
-    start = start + ROWS
-    aFile = File.new("TVNews_results"+ start.to_s + ".json", "w")
-    aFile.write(results)
-    json_data += JSON.parse(results)
-    aFile.close
-  else 
-    print "request failed"
-# hang out for a minute before trying again
-    sleep(60)
+  # set variables
+  start = 0
+  rows = 200
+
+  json_data = []
+
+  # incrementally download JSON files
+  while start < 1000
+  # don't forget to turn variable numbers into strings with .to_s
+    url = "http://archive.org/details/tv?q=%22" + search_term + "%22&time=20120917-20121112&start=" + start.to_s + "&rows=" + rows.to_s + "&output=json"
+    print "fetching from " + start.to_s + "\n"
+    print "  "+url + "\n"
+
+    file = open(url)
+    results = file.read
+    print "  got "+results.length.to_s+" characters\n"
+   
+    #openurl library and pass url to fetch it
+    #if success
+     # increment start row +100
+      #save to disk with filename results startrow#.json
+    #else 
+     # wait one minute and do it again
+
+  # if page doesn't load...
+    if results.index("Our Search Engine was not responsive.") == nil
+      start = start + rows
+      path = "./" + search_term
+      FileUtils.mkdir_p(path) unless File.exists?(path)
+      aFile = File.new(path + "/TVNews_results"+ start.to_s + ".json", "w")
+      aFile.write(results)
+      aFile.close
+    else 
+      print "request failed"
+  # hang out for a minute before trying again
+      sleep(60)
+    end
   end
-end 
+end
+
+write_to_file
