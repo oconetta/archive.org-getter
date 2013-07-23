6894ab0f12eece3d40926d072740c1336b9aad90
Olivia Conetta <olivia.conetta@gmail.com>
2013-07-23 16:36:27 -0400

    Delete test_script.rb

--- test_script.rb
+++
@@ -1,4 +0,0 @@
-#my_string = 'RT @CalWatchdog: NEW: Cal State illegally promotes Prop. 30 tax increase http://t.co/yXD1Vdnu'
-#if my_string.include?('http://t.co/')
-#	puts my_string.slice('http://t.co/')
-#end
