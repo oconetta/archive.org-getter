1d19f40701b9e6eef003d4937f3ba1cc529d0492
oconetta <olivia.conetta@gmail.com>
2013-07-24 14:49:20 -0400

    Dates and snippets can be written to CSV

--- tv_snippets.rb
+++ tv_snippets.rb
@@ -1,6 +1,6 @@
-require 'rubygems'
 require 'json'
-require 'pp'
+require 'fileutils'
+require 'csv'
 
 #parse data from JSON files and store in array of hashes
 tv_data = JSON.parse(File.read('TVNews_results200.json'))
@@ -72,6 +72,12 @@ snippets.each do |snip|
 	end
 end
 
+#if the # of items in dates != the # of items in snippets...
+if dates.length != snippets.length
+  #abort, because the rest of the program won't work!
+  abort("We don't have the same amount of dates and snippets!")
+end
+
 #create hash with dates as keys and snippets as values
 dates_and_snips = Hash.new(0)
 
@@ -82,5 +88,17 @@ while count < dates.length
   count += 1
 end
 
-#print the hash
-pp dates_and_snips
+puts 'Which ballot proposition are you working on? (Type a number.)'
+ballot_prop = gets.chomp
+
+#output hash to JSON
+path = FileUtils.pwd
+FileUtils.mkdir_p(path) unless File.exists?(path)
+new_file = File.new(path + '/Dates_and_Snippets_for_Prop' + ballot_prop.to_s + '.csv', 'w')
+csv_string = CSV.generate do |csv|
+  dates_and_snips.each do |key, value|
+    csv << [key, value]
+  end
+end
+new_file.write(csv_string)
+new_file.close
